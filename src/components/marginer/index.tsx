8a158790dd6553f29c140478bf3951c5dd0202c3
Guigui14460 <letgui2@gmail.com>
2022-04-18 23:24:07 +0200

    update: better direction of marginer

--- src/components/marginer/index.tsx
+++ src/components/marginer/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 
+type MarginerDirection = 'horizontal' | 'vertical';
+
 const HorizontalMargin = styled.span<{ margin: string|number }>`
   display: flex;
   width: ${({ margin }) =>
@@ -13,7 +15,7 @@ const VerticalMargin = styled.span<{ margin: string|number }>`
     typeof margin === "string" ? margin : `${margin}px`};
 `;
 
-const Marginer = (props: {direction: string, margin: number|string}) => {
+const Marginer = (props: {direction: MarginerDirection, margin: number|string}) => {
   const { direction } = props;
 
   if (direction === "horizontal") return <HorizontalMargin {...props} />;
