c1a2e550c7430e63e4d4c6581dee6247fee5d1e7
Guigui14460 <letgui2@gmail.com>
2022-04-20 15:47:46 +0200

    fix: load efficiency

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,66 @@
 import './index.css';
+import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
-import HomePage from "./pages/home";
-import ProjectsPage from "./pages/projects";
-import AboutPage from "./pages/about";
-import ContactPage from "./pages/contact";
-import SkillsPage from "./pages/skills";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import ScrollToTop from "./ScrollToTop";
+import { DotsLoader } from './components/loader';
+const HomePageComponent = lazy(() => import("./pages/home"));
+const ProjectsPageComponent = lazy(() => import("./pages/projects"));
+const AboutPageComponent = lazy(() => import("./pages/about"));
+const ContactPageComponent = lazy(() => import("./pages/contact"));
+const SkillsPageComponent = lazy(() => import("./pages/skills"));
+
+const ComponentLoader = () => {
+    return <div style={{ display: "flex", justifyContent: "center" }}>
+        <DotsLoader />
+    </div>
+}
+
+const Page = (props: { element: JSX.Element }) => {
+    return <div>
+        <Suspense fallback={<ComponentLoader />}>
+            {props.element}
+        </Suspense>
+    </div>
+}
+
+// const HomePage = () => {
+//     return <div>
+//         <Suspense fallback={<ComponentLoader />}>
+//             <HomePageComponent />
+//         </Suspense>
+//     </div>
+// }
+// const ProjectsPage = () => {
+//     return <div>
+//         <Suspense fallback={<ComponentLoader />}>
+//             <ProjectsPageComponent />
+//         </Suspense>
+//     </div>
+// }
+// const AboutPage = () => {
+//     return <div>
+//         <Suspense fallback={<ComponentLoader />}>
+//             <AboutPageComponent />
+//         </Suspense>
+//     </div>
+// }
+// const ContactPage = () => {
+//     return <div>
+//         <Suspense fallback={<ComponentLoader />}>
+//             <ContactPageComponent />
+//         </Suspense>
+//     </div>
+// }
+// const SkillsPage = () => {
+//     return <div>
+//         <Suspense fallback={<ComponentLoader />}>
+//             <SkillsPageComponent />
+//         </Suspense>
+//     </div>
+// }
 
 function App() {
   return (
@@ -17,11 +69,16 @@ function App() {
       <Header />
       <main className="container">
         <Switch>
-          <Route path="/" exact component={HomePage} />
+          <Route path="/" exact component={() => <Page element={<HomePageComponent />} />} />
+          <Route path="/about" exact component={() => <Page element={<AboutPageComponent />} />} />
+          <Route path="/contact" exact component={() => <Page element={<ContactPageComponent />} />} />
+          <Route path="/projects" exact component={() => <Page element={<ProjectsPageComponent />} />} />
+          <Route path="/skills" exact component={() => <Page element={<SkillsPageComponent />} />} />
+          {/* <Route path="/" exact component={HomePage} />
           <Route path="/about" exact component={AboutPage} />
           <Route path="/contact" exact component={ContactPage} />
           <Route path="/projects" exact component={ProjectsPage} />
-          <Route path="/skills" exact component={SkillsPage} />
+          <Route path="/skills" exact component={SkillsPage} /> */}
           <Route render={() => <Redirect to="/" />} />
         </Switch>
       </main>
