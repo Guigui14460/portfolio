fb10b05f4565563b382017c8e3a72bb3d8f3c87d
Guigui14460 <letgui2@gmail.com>
2022-04-22 11:10:44 +0200

    add: tab component

---
+++ src/components/tabs/index.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import "./tabs.css"
+
+export const TabPanel = (props: { title: string; children: React.ReactNode }) => {
+    return <div>{props.children}</div>
+};
+
+export const Tab = (props: { active?: number; children: React.ReactNode | React.ReactNode[] }) => {
+    const [activeTab, setActiveTab] = useState(props.active || 0);
+    const [tabsData, setTabsData] = useState<{title: string; children: JSX.Element}[]>([]);
+
+    useEffect(() => {
+        let data: {title: string; children: JSX.Element}[] = [];
+
+        React.Children.forEach(props.children, (element) => {
+            if(!React.isValidElement(element)) return;
+
+            const {props: {title, children}} = element;
+            data.push({ title, children });
+        });
+
+        setTabsData(data);
+    }, [props.children]);
+
+    return <div className="tab">
+        <ul className="tab__list">
+            {tabsData.map((value, index) => {
+                const style = index === activeTab ? "selected" : "";
+                return <li key={index} className={`tab__list-item ${style}`} onClick={() => setActiveTab(index)}>
+                    {value.title}
+                </li>
+            })}
+        </ul>
+        <div className="tab__content">{tabsData[activeTab] && tabsData[activeTab].children}</div>
+    </div>
+};
